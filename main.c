39de5fd3a6836fe293c4e84965efa49d93d3fb18
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-04-07 16:03:26 -0300

    next_token and new_lexer

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TODO(name) \
+    do { \
+        fprintf(stderr, "\"%s\" isn't ready to use.\n", name); \
+        exit(1); \
+    } while(0); \
+
+bool is_aplha(char c) {
+    TODO("is_aplha");
+    return false;
+}
 
 typedef enum {
     TOKEN_END = 0,
+    TOKEN_PREPRO,
 } TokenType;
+
+const char *token_type_to_string(TokenType type) {
+
+    switch (type) {
+        case TOKEN_END:
+            return "end of the token";
+        case TOKEN_PREPRO:
+            return "pre-process token";
+    }
+}
 
 typedef struct {
     const char *buffer;
@@ -27,7 +52,36 @@ Lexer new_lexer(const char* buffer, size_t len) {
     return lexer;
 }
 
+Token next_token(Lexer *lexer) {
+    Token token = {0};
+
+    if (lexer->cursor >= lexer->len) {
+        token.len = 1;
+        token.buffer = &lexer->buffer[lexer->cursor];
+    }
+    else if (lexer->buffer[lexer->cursor] == '#') {
+        token.type = TOKEN_PREPRO;
+        token.buffer = &lexer->buffer[lexer->cursor];
+
+        while (is_aplha(lexer->buffer[lexer->cursor]) && lexer->cursor <= lexer->len) {
+            token.len += 1;
+            lexer->cursor += 1;
+        }
+    }
+
+    return token;
+}
+
 int main(void) {
     const char *input = "#include <stdio.h>";
+
+    Lexer lexer = new_lexer(input, strlen(input));
+    Token token = next_token(&lexer);
+
+    while (token.type != TOKEN_END) {
+        printf("%*.s: %s\n", (int)token.len, token.buffer, token_type_to_string(token.type));
+        token = next_token(&lexer);
+    }
+
     return 0;
 }
