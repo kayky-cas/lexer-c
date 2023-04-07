d0c5975e6f3d2160bb244573c090a2e6a55b1981
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-04-07 19:05:46 -0300

    starting tokenizations

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -11,8 +12,9 @@
     } while(0); \
 
 bool is_aplha(char c) {
-    TODO("is_aplha");
-    return false;
+    return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= '0' && c <= '9');
 }
 
 typedef enum {
@@ -52,22 +54,34 @@ Lexer new_lexer(const char* buffer, size_t len) {
     return lexer;
 }
 
+#define END(lexer) lexer->cursor <= lexer->len
+#define CURSOR(lexer) lexer->buffer[lexer->cursor]
+
 Token next_token(Lexer *lexer) {
-    Token token = {0};
+    Token token = {
+        .type = TOKEN_END
+    };
 
     if (lexer->cursor >= lexer->len) {
         token.len = 1;
-        token.buffer = &lexer->buffer[lexer->cursor];
+        token.buffer = &CURSOR(lexer);
     }
-    else if (lexer->buffer[lexer->cursor] == '#') {
-        token.type = TOKEN_PREPRO;
-        token.buffer = &lexer->buffer[lexer->cursor];
 
-        while (is_aplha(lexer->buffer[lexer->cursor]) && lexer->cursor <= lexer->len) {
+    else if (CURSOR(lexer) == '#') {
+        token.type = TOKEN_PREPRO;
+        token.buffer = &CURSOR(lexer);
+        token.len = 1;
+
+        lexer->cursor += 1;
+
+        while (is_aplha(CURSOR(lexer)) && END(lexer)) {
             token.len += 1;
             lexer->cursor += 1;
         }
+    } else if (CURSOR(lexer) == '<') {
+    } else if (CURSOR(lexer) == '>') {
     }
+
 
     return token;
 }
@@ -79,7 +93,7 @@ int main(void) {
     Token token = next_token(&lexer);
 
     while (token.type != TOKEN_END) {
-        printf("%*.s: %s\n", (int)token.len, token.buffer, token_type_to_string(token.type));
+        printf("%.*s: %s\n", (int) token.len, token.buffer, token_type_to_string(token.type));
         token = next_token(&lexer);
     }
 
