84275e6fa0a8640a5fae05f369093dc25fdbdc00
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-04-07 15:35:42 -0300

    First struct model

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
+#include <stddef.h>
 #include <stdio.h>
 
+typedef enum {
+    TOKEN_END = 0,
+} TokenType;
+
+typedef struct {
+    const char *buffer;
+    size_t len;
+    TokenType type;
+} Token;
+
+typedef struct {
+    const char* buffer;
+    size_t len;
+    size_t cursor;
+} Lexer;
+
+Lexer new_lexer(const char* buffer, size_t len) {
+    Lexer lexer = {
+        .buffer = buffer,
+        .len = len,
+        .cursor = 0
+    };
+
+    return lexer;
+}
+
 int main(void) {
+    const char *input = "#include <stdio.h>";
     return 0;
 }
